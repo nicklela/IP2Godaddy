5ef2be659cfdc9b7f103d29b9ef1e9b39c2fa0bd
nicklela <nicklle@gmail.com>
2021-03-09 16:13:25 +0800

    add config class and logging mechanism

--- config.py
+++ config.py
@@ -1,5 +1,19 @@
 import json
+import logging
+import os
 
 class Configuration:
-    pass
+    DEFAULT_CONFIG = "config.json.default"
+    file_path = ""
+    settings = {}
 
+    def __init__(self, config = DEFAULT_CONFIG):
+        self.file_path=config
+
+    def load(self):
+        with open(self.file_path, 'r') as json_file:
+            self.settings = json.load(json_file)
+
+    def dump(self):
+        logging.info('Configuration file: ' + self.file_path)
+        logging.info('Configuration: ' + json.dumps(self.settings))
--- ip2godaddy.py
+++ ip2godaddy.py
@@ -1,4 +1,13 @@
 import argparse
+import logging
+from config import Configuration
+
+"""
+Debug log setting
+"""
+def debug_enabled():
+    FORMAT = '%(asctime)s %(levelname)s: %(message)s'
+    logging.basicConfig(level=logging.DEBUG, format=FORMAT)
 
 """
 Main function
@@ -8,11 +17,27 @@ def main():
     parse user input
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--config", help="specify the config.json in other place", default="config.json")
-
+    parser.add_argument("-c", "--config", help="specify the config.json in other place", default=Configuration.DEFAULT_CONFIG)
     args = parser.parse_args()
-    print(args.config)
+
+    """
+    Get config
+    """
+    config = Configuration(args.config)
+    try:
+        config.load()
+    except Exception as e:
+        logging.error('Fail to load configuration from ' + config.file_path + ' due to {0}'.format(e))
+        raise RuntimeError('Fail to load configuration') from e
+
+    config.dump()
+
+    """
+    Update DNS
+    """
 
 if __name__ == "__main__":
+
+    debug_enabled()
     main()
 
